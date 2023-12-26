0c472754889161a4bbea16a04b057daf13f42c39
tzivya98 <tzivya7@gmail.com>
2023-12-26 12:40:43 +0200

    add html

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import index from './index.js';
+import index1 from './index1.html'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
